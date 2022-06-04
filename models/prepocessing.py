1558f4d9ac885d85169e2fd76e677411dab79393
Iva Ištuk <ivaistuk@gmail.com>
2022-06-04 22:43:04 +0200

    emoji preprocessing added

---
+++ models/prepocessing.py
@@ -0,0 +1,121 @@
+import io
+import re
+import emoji
+
+label2emotion = {0: "others", 1: "happy", 2: "sad", 3: "angry"}
+emotion2label = {"others": 0, "happy": 1, "sad": 2, "angry": 3}
+
+def preprocessEmojiData(dataFilePath, mode):
+    """
+    Load data from a file, process and return indices, conversations and labels in separate lists
+    Input:
+        dataFilePath : Path to train/test file to be processed
+        mode : "train" mode returns labels. "test" mode doesn't return labels.
+    Output:
+        indices : Unique conversation ID list
+        conversations : List of 3 turn conversations, processed and each turn separated by the <eos> tag
+        labels : [Only available in "train" mode] List of labels
+    """
+    indices = []
+    conversations = []
+    labels = []
+    with io.open(dataFilePath, encoding="utf8") as finput:
+        finput.readline()
+        for line in finput:
+            # Convert multiple instances of . ? ! , to single instance
+            # okay...sure -> okay . sure
+            # okay???sure -> okay ? sure
+            # Add whitespace around such punctuation
+            # okay!sure -> okay ! sure
+            line = emoji.demojize(line, delimiters=("", " "))
+            repeatedChars = ['.', '?', '!', ',']
+            for c in repeatedChars:
+                lineSplit = line.split(c)
+                while True:
+                    try:
+                        lineSplit.remove('')
+                    except:
+                        break
+                if c == "_":
+                    cSpace = ' '
+                else:
+                    cSpace = ' ' + c + ' '
+                line = cSpace.join(lineSplit)
+
+            line = line.strip().split('\t')
+            if mode == "train":
+                # Train data contains id, 3 turns and label
+                label = emotion2label[line[4]]
+                labels.append(label)
+
+            conv = ' <eos> '.join(line[1:4])
+
+            # Remove any duplicate spaces
+            duplicateSpacePattern = re.compile(r'\s+')
+            conv = re.sub(duplicateSpacePattern, ' ', conv)
+
+            indices.append(int(line[0]))
+            conversations.append(conv.lower())
+
+    if mode == "train":
+        return indices, conversations, labels
+    else:
+        return indices, conversations
+
+
+def preprocessData(dataFilePath, mode):
+    """
+    Load data from a file, process and return indices, conversations and labels in separate lists
+    Input:
+        dataFilePath : Path to train/test file to be processed
+        mode : "train" mode returns labels. "test" mode doesn't return labels.
+    Output:
+        indices : Unique conversation ID list
+        conversations : List of 3 turn conversations, processed and each turn separated by the <eos> tag
+        labels : [Only available in "train" mode] List of labels
+    """
+    indices = []
+    conversations = []
+    labels = []
+    with io.open(dataFilePath, encoding="utf8") as finput:
+        finput.readline()
+        for line in finput:
+            # Convert multiple instances of . ? ! , to single instance
+            # okay...sure -> okay . sure
+            # okay???sure -> okay ? sure
+            # Add whitespace around such punctuation
+            # okay!sure -> okay ! sure
+            line = emoji.demojize(line, delimiters=("", " "))
+            repeatedChars = ['.', '?', '!', ',']
+            for c in repeatedChars:
+                lineSplit = line.split(c)
+                while True:
+                    try:
+                        lineSplit.remove('')
+                    except:
+                        break
+                if c == "_":
+                    cSpace = ' '
+                else:
+                    cSpace = ' ' + c + ' '
+                line = cSpace.join(lineSplit)
+
+            line = line.strip().split('\t')
+            if mode == "train":
+                # Train data contains id, 3 turns and label
+                label = emotion2label[line[4]]
+                labels.append(label)
+
+            conv = ' <eos> '.join(line[1:4])
+
+            # Remove any duplicate spaces
+            duplicateSpacePattern = re.compile(r'\s+')
+            conv = re.sub(duplicateSpacePattern, ' ', conv)
+
+            indices.append(int(line[0]))
+            conversations.append(conv.lower())
+
+    if mode == "train":
+        return indices, conversations, labels
+    else:
+        return indices, conversations
